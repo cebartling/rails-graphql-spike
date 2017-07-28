3f7e027cec090ae3ac0aef7c52b97f85b5461847
Christopher Bartling <chris.bartling@gmail.com>
2017-07-28 15:28:39 -0500

    Fixed issue with flow files.

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -41,7 +41,8 @@ module.exports = {
     new ManifestPlugin({
       publicPath: output.publicPath,
       writeToFileEmit: true
-    })
+    }),
+    new webpack.ContextReplacementPlugin(/graphql-language-service-interface[\/\\]dist/, /\.js$/),
   ],
 
   resolve: {
--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 npm install -g @2fd/graphdoc
+npm install -g flow-bin
+
